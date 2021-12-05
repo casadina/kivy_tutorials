316eec88998fd30798369c1cb388032bb3d9c596
Casadina <casadina@tuta.io>
2021-12-05 20:42:20 +0800

    Edit hello_kivy.py so that the button will call a function on press.   -Create subclass of Button called NewButton that will return a string instead of object instance.

--- hello_kivy.py
+++ hello_kivy.py
@@ -2,17 +2,27 @@
 """hello_kivy.py - Simple kivy program."""
 
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.button import Button
+
+
+class NewButton(Button):
+    def __repr__(self):
+        return 'This button greets you.'
 
 
 class MainApp(App):
     """Displays hello message."""
-    def build(self) -> Label:
-        label = Label(text='Hello from Kivy',
-                      size_hint=(0.5, 0.5),
-                      pos_hint={'center_x': 0.5, 'center_y': 0.5})
+    def build(self) -> NewButton:
+        button = NewButton(text='Hello from Kivy',
+                           size_hint=(0.5, 0.5),
+                           pos_hint={'center_x': 0.5, 'center_y': 0.5})
+        button.bind(on_press=self.on_press_button)
+        return button
 
-        return label
+    @staticmethod
+    def on_press_button(instance):
+        print(instance)
+        print('You pressed the button!')
 
 
 if __name__ == "__main__":
